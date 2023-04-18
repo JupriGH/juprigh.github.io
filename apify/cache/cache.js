efa69d5fa64725f25131d457502d0f1db73b0cfa
JupriGH <pabuaran@gmail.com>
2023-04-18 20:20:11 +0700

    updates

--- apify/cache/cache.js
+++ apify/cache/cache.js
@@ -9,14 +9,15 @@ window.on('load', e => {
 		port2.onmessage = async (e) => {
 			console.log('CHANNEL: FROM MAIN', e)
 			switch(e.data.type) {
+
 			case 'get':
 				var rec = await app.db.get_data(e.data.id)
-				console.log('RECORD', rec)
 				port2.postMessage(rec)
 				break
 				
 			case 'set':
-				console.log('SET', e)
+				var {id, data, meta} = e.data
+				if (id && data && meta) await app.db.set_data(id, data, meta)
 				break
 			}
 		}
